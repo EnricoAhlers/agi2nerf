6df283549cb44469593cfd4e6bee9aaa0f4568c0
Josiah Reeves <82180221+joreeves@users.noreply.github.com>
2023-04-12 18:51:15 -0700

    bounding box and scaling update

--- agi2nerf.py
+++ agi2nerf.py
@@ -2,109 +2,135 @@
 import argparse
 import xml.etree.ElementTree as ET
 import math
-import cv2
 import numpy as np
 from copy import deepcopy as dc
 
-# Check if matplotlib is installed
-try:
-	import matplotlib.pyplot as plt
-	_plt = True
-except:
-	print("matplotlib not installed, not plotting")
-	_plt = False
+from utils import agiMat2Nerf, sharpness, central_point, plot, _plt
 
 import json
 
 from tqdm import tqdm
 from pathlib import Path
 
-###############################################################################
-# START
-# code taken from https://github.com/NVlabs/instant-ngp
-# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
-def closest_point_2_lines(oa, da, ob, db): # returns point closest to both rays of form o+t*d, and a weight factor that goes to 0 if the lines are parallel
-	da = da / np.linalg.norm(da)
-	db = db / np.linalg.norm(db)
-	c = np.cross(da, db)
-	denom = np.linalg.norm(c)**2
-	t = ob - oa
-	ta = np.linalg.det([t, db, c]) / (denom + 1e-10)
-	tb = np.linalg.det([t, da, c]) / (denom + 1e-10)
-	if ta > 0:
-		ta = 0
-	if tb > 0:
-		tb = 0
-	return (oa+ta*da+ob+tb*db) * 0.5, denom
-
-def central_point(out):
-	# find a central point they are all looking at
-	print("computing center of attention...")
-	totw = 0.0
-	totp = np.array([0.0, 0.0, 0.0])
-	for f in out["frames"]:
-		mf = np.array(f["transform_matrix"])[0:3,:]
-		for g in out["frames"]:
-			mg = g["transform_matrix"][0:3,:]
-			p, w = closest_point_2_lines(mf[:,3], mf[:,2], mg[:,3], mg[:,2])
-			if w > 0.01:
-				totp += p*w
-				totw += w
-
-	totp /= totw
-	print("The center of attention is:{}".format(totp)) # the cameras are looking at totp
-
-	for f in out["frames"]:
-		f["transform_matrix"][0:3,3] -= totp
-		f["transform_matrix"] = f["transform_matrix"].tolist()
-		
-	return out
-
-def sharpness(imagePath):
-	image = cv2.imread(imagePath)
-	if image is None:
-		print("Image not found:", imagePath)
-		return 0
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	fm = cv2.Laplacian(gray, cv2.CV_64F).var()
-	return fm
-
-#END
-###############################################################################
-###############################################################################
-# START
-# Copyright (C) 2022, Enrico Philip Ahlers. All rights reserved.
-
 
 def parse_args():
 	parser = argparse.ArgumentParser(description="convert Agisoft XML export to nerf format transforms.json")
 
-	parser.add_argument("--xml_in", default="", help="specify xml file location")
-	parser.add_argument("--out", default="transforms.json", help="output path")
+	parser.add_argument("--xml_in", help="specify xml file location") #TODO: Chang to positional argument
+	parser.add_argument("--out", dest="path", default="transforms.json", help="output path")
 	parser.add_argument("--imgfolder", default="./images/", help="location of folder with images")
 	parser.add_argument("--imgtype", default="jpg", help="type of images (ex. jpg, png, ...)")
+	parser.add_argument("--aabb_scale", default=16, type=int, help="size of the aabb, default is 16")
+	parser.add_argument("--plot", action="store_true", help="plot the cameras and the bounding region in 3D")
+	parser.add_argument("--scale", default=1.0, type=float, help="scale the scene by a factor")
+	parser.add_argument("--no_scale", action="store_true", help="DISABLES the scaling of the cameras to the bounding region")
+	parser.add_argument("--no_center", action="store_true", help="DISABLES the centering of the cameras around the computed central point")
+	parser.add_argument("--camera_size", default=0.1, type=float, help="size of the camera in the 3D plot. Does not affect the output.")
+	
+	parser.add_argument("--debug_ignore_images", action="store_true", help="IGNORES the images in the xml file. For debugging purposes only.")
+	
 	args = parser.parse_args()
 	return args
 
-def reflectZ():
-	return [[1, 0, 0, 0],
-			[0, 1, 0, 0],
-			[0, 0, -1, 0],
-			[0, 0, 0, 1]]
 
-def reflectY():
-	return [[1, 0, 0, 0],
-			[0, -1, 0, 0],
-			[0, 0, 1, 0],
-			[0, 0, 0, 1]]
+def parse_region(xml_root):
+	"""
+	Parse the region xml
+	The xml is formatted as follows:
 
-def matrixMultiply(mat1, mat2):
-	return np.array([[sum(a*b for a,b in zip(row, col)) for col in zip(*mat2)] for row in mat1])
+	<region>
+		<center>0 0 0.5</center>
+		<size>1 1 1</size>
+		<R>1 0 0 0 1 0 0 0 1</R>
+    </region>
+	"""
+	region = xml_root.find('.//region')
+	
+	center = np.array([float(i) for i in region.find('center').text.split()])
+	size = np.array([float(i) for i in region.find('size').text.split()])
+	rotation = np.array([float(i) for i in region.find('R').text.split()]).reshape(3,3)
 
-#END
-###############################################################################
+	mat = np.eye(4)
+	mat[:3,:3] = rotation.T # Why transpose? Don't ask questions...
+	mat[:3,3] = center
+	
+	return mat, size
 
-# Copyright (C) 2023, Josiah David Reeves. All rights reserved.
+
+def parse_components(xml_root):
+	"""
+	Parse the transform and region from components of the xml
+	The xml is formatted as follows:
+    <components next_id="1" active_id="0">
+      <component id="0" label="Component 1">
+		<transform>
+		<rotation locked="true">1 0 0 0 1 0 0 0 1</rotation>
+		<translation locked="true">0 0 0</translation>
+		<scale locked="true">1</scale>
+		</transform>
+		<region>
+			<center>0 0 0.5</center>
+			<size>1 1 1</size>
+			<R>1 0 0 0 1 0 0 0 1</R>
+		</region>
+      </component>
+    </components>
+	"""
+	# http://wiki.agisoft.com/wiki/Coordinate_System_to_Bounding_Box.py
+	# https://www.agisoft.com/forum/index.php?topic=6176.0
+
+	comp = xml_root.find('.//components/component')
+
+	if comp is None:
+		return None
+	
+	xml_tform = comp.find('transform')
+	xml_region = comp.find('region')
+
+	if xml_tform is None:
+		scene = (None, None)
+	else:
+		rotation = np.array([float(i) for i in xml_tform.find('rotation').text.split()]).reshape(3,3)
+		translation = np.array([float(i) for i in xml_tform.find('translation').text.split()])
+		scale = np.array([float(i) for i in xml_tform.find('scale').text.split()])
+
+		mat = np.eye(4)
+		mat[:3,:3] = rotation
+		mat[:3,3] = translation
+		# mat = pytr.scale_transform(mat, s_d=scale)
+		scene = mat, scale
+
+	if xml_region is None:
+		region = (None, None)
+	else:
+		center = np.array([float(i) for i in xml_region.find('center').text.split()])
+		size = np.array([float(i) for i in xml_region.find('size').text.split()])
+		rotation = np.array([float(i) for i in xml_region.find('R').text.split()]).reshape(3,3)
+
+		mat = np.eye(4)
+		mat[:3,:3] = rotation.T # Why transpose? Don't ask questions...
+		mat[:3,3] = center
+
+		region = mat, size
+	
+	print(scene, region)
+	return scene, region
+
+
+def parse_xform(cam):
+	xform = cam.find('transform')
+
+	# These are unused right now
+	# rotation_covariance = cam.find('rotation_covariance')
+	# location_covariance = cam.find('location_covariance')
+
+	if xform is None:
+		return None
+	
+	mat = np.array([float(i) for i in xform.text.split()]).reshape(4,4)
+
+	return mat
+
 
 def parse_camera(cam):
 
@@ -115,23 +141,18 @@ def parse_camera(cam):
 		return None
 	
 	# Get the camera label and sensor id
-	label = cam.get("label")#.split('_')[3]
+	# So we can match the sensor to the camera
+	label = cam.get("label")
 	sensor_id = cam.get("sensor_id")
 
 	current_camera = dict()
-	current_camera.update({"label":label})
-	current_camera.update({"sensor_id":int(sensor_id)})
-	matrix_elements = [float(i) for i in cam[0].text.split()]
-	transform_matrix = np.array([[matrix_elements[0], matrix_elements[1], matrix_elements[2], matrix_elements[3]], [matrix_elements[4], matrix_elements[5], matrix_elements[6], matrix_elements[7]], [matrix_elements[8], matrix_elements[9], matrix_elements[10], matrix_elements[11]], [matrix_elements[12], matrix_elements[13], matrix_elements[14], matrix_elements[15]]])
-	
-	#swap axes
-	transform_matrix = transform_matrix[[2,0,1,3],:]
-
-	#reflect z and Y axes
-	current_camera.update({"transform_matrix":matrixMultiply(matrixMultiply(transform_matrix, reflectZ()), reflectY())} )
+	current_camera['label'] = label
+	current_camera['sensor_id'] = int(sensor_id)
+	current_camera['transform_matrix'] = parse_xform(cam)
 	
 	return current_camera
-	
+
+
 def parse_sensor(sensor):
 	out = dict()
 
@@ -147,21 +168,22 @@ def parse_sensor(sensor):
 
 	if (calib is None):
 		print("No calibration found for sensor {}".format(id))
-		# Calculate the focal if not provided
-
+		
 		# Get the sensor resolution
 		res = sensor.find("resolution")
 		w = float(res.get('width'))
 		h = float(res.get('height'))
 
-		# Get the pixel width and height
-		# The xml is formatted as follows:
-		# <sensor id="" label="" type="frame">
-		# 	<resolution width="4000" height="6000"/>
-		# 	<property name="pixel_width" value=""/>
-		# 	<property name="pixel_height" value="0.0039083244579612101"/>
-		# 	<property name="focal_length" value="55"/>
-		# 	<property name="layer_index" value="0"/>
+		"""	
+		Get the pixel width and height
+		The xml is formatted as follows:
+		<sensor id="" label="" type="frame">
+			<resolution width="4000" height="6000"/>
+			<property name="pixel_width" value=""/>
+			<property name="pixel_height" value="0.0039083244579612101"/>
+			<property name="focal_length" value="55"/>
+			<property name="layer_index" value="0"/>
+		"""
 
 		properties = sensor.findall('property')
 		pixel_width = float(properties[0].get('value'))
@@ -177,18 +199,12 @@ def parse_sensor(sensor):
 		camera_angle_x = math.atan(float(w) / (float(fl_pxl) * 2)) * 2
 		camera_angle_y = math.atan(float(h) / (float(fl_pxl) * 2)) * 2
 
-		out.update({"camera_angle_x": camera_angle_x})
-		out.update({"camera_angle_y": camera_angle_y})
-		out.update({"fl_x": fl_pxl})
-		out.update({"fl_y": fl_pxl})
-		# out.update({"k1": 0})
-		# out.update({"k2": 0})
-		# out.update({"p1": 0})
-		# out.update({"p2": 0})
-		# out.update({"cx": 0})
-		# out.update({"cy": 0})
-		out.update({"w": w})
-		out.update({"h": h})
+		out["camera_angle_x"] = camera_angle_x
+		out["camera_angle_y"] = camera_angle_y
+		out["fl_x"] = fl_pxl
+		out["fl_y"] = fl_pxl
+		out["w"] = w
+		out["h"] = h
 	else:
 		res = calib.find("resolution")
 		w = float(res.get('width'))
@@ -197,36 +213,33 @@ def parse_sensor(sensor):
 		fl_x = float(calib.find('f').text)
 		fl_y = fl_x
 		
-		k1 = float(calib.find('k1').text if calib.find('k1') is not None else 0)
-		k2 = float(calib.find('k2').text if calib.find('k2') is not None else 0)
-		k3 = float(calib.find('k3').text if calib.find('k3') is not None else 0)
-		k4 = float(calib.find('k4').text if calib.find('k4') is not None else 0)
-		p1 = float(calib.find('p1').text if calib.find('p1') is not None else 0)
-		p2 = float(calib.find('p2').text if calib.find('p2') is not None else 0)
+		k1 = float(calib.find('k1').text if calib.find('k1') is not None else -1)
+		k2 = float(calib.find('k2').text if calib.find('k2') is not None else -1)
+		p1 = float(calib.find('p1').text if calib.find('p1') is not None else -1)
+		p2 = float(calib.find('p2').text if calib.find('p2') is not None else -1)
 		cx = float(calib.find('cx').text if calib.find('cx') is not None else 0) + w/2
 		cy = float(calib.find('cy').text if calib.find('cy') is not None else 0) + h/2
 
 		camera_angle_x = math.atan(float(w) / (float(fl_x) * 2)) * 2
 		camera_angle_y = math.atan(float(h) / (float(fl_y) * 2)) * 2
 		
-		out.update({"camera_angle_x": camera_angle_x})
-		out.update({"camera_angle_y": camera_angle_y})
-		out.update({"fl_x": fl_x})
-		out.update({"fl_y": fl_y})
-		out.update({"k1": k1})
-		out.update({"k2": k2})
-		out.update({"k3": k3})
-		out.update({"k4": k4})
-		out.update({"p1": p1})
-		out.update({"p2": p2})
-		out.update({"cx": cx})
-		out.update({"cy": cy})
-		out.update({"w": w})
-		out.update({"h": h})
+		out["camera_angle_x"] = camera_angle_x
+		out["camera_angle_y"] = camera_angle_y
+		out["fl_x"] = fl_x
+		out["fl_y"] = fl_y
+		out["k1"] = k1
+		out["k2"] = k2
+		out["p1"] = p1
+		out["p2"] = p2
+		out["cx"] = cx
+		out["cy"] = cy
+		out["w"] = w
+		out["h"] = h
 		
 	return out
 	
-def calibration(root):
+
+def calibration(root, stems, _scale=1.0, _no_scale=False, _ignore_images=False):
 	'''
 	Take the xml file and generate the calibration data
 
@@ -254,77 +267,62 @@ def calibration(root):
 
 	sensors = root.findall('.//sensor')
 	cameras = root.findall('.//camera')
-	
-	# print("Found {} sensors and {} cameras".format(len(sensors), len(cameras)))
-	
+
 	sensors = [parse_sensor(s) for s in sensors]
 	cameras = [parse_camera(c) for c in cameras]
+
+	# Remove empty cameras
+	cameras = [c for c in cameras if c]
+
+	# Transform the cameras to the component's coordinate system
+	scene, region = parse_components(root)
+
+	scene_mat, scene_scale = scene
+	region_mat, region_scale = region
+	
+	scene_scale = scene_scale if not _no_scale else 1.0
+	scene_scale *= _scale
+
+	for c in cameras:
+		M = c['transform_matrix']
+
+		if (scene_mat is not None) & (scene_scale is not None):
+			M[:3,3] = M[:3,3] * scene_scale
+			M = np.dot(scene_mat, M)
+		
+		c['transform_matrix'] = agiMat2Nerf(M)
 
 	calib = []
 
 	# Match sensors to cameras
 	#TODO: There's probably a better way to do this...
 	for c in cameras:
-		if not c:
-			continue
-
 		for s in sensors:
 			if(c['sensor_id'] == s['id']):
-				# print(c['sensor_id'], s['id'])
 				calib.append((dc(c), dc(s)))
 				break
 	
-	print("Found {} cameras with calibration data".format(len(calib)))
+	print("\nFound {} cameras and {} sensors".format(len(cameras), len(sensors)))
+	print("\nFound {} matching cameras and sensors".format(len(calib)))
 
-	return calib
+	frames = []
 
-if __name__ == "__main__":
-	args = parse_args()
-	XML_LOCATION = args.xml_in
-	IMGTYPE = args.imgtype
-	IMGFOLDER = args.imgfolder
+	pbar = tqdm(total=len(root[0][2]))
 
-	files = list(Path(IMGFOLDER).glob('*.{}'.format(IMGTYPE)))
-	stems = list([f.stem for f in files])
+	for camera, sensor in calib:
+		pbar.update(1)
 
-	# Check if the files path has images in it
-	if(len(files)==0):
-		print('No images found in folder: {}'.format(IMGFOLDER))
-		exit()
+		if (camera is None) or (sensor is None):
+			print('No camera or sensor found')
+			continue
 
-	out = dict()
-
-	aabb_scale = 16
-
-	out.update({"aabb_scale": aabb_scale})
-
-	with open(XML_LOCATION, "r") as f:
-		root = ET.parse(f).getroot()
-		
-		# See issue for multi camera support
-		# https://github.com/NVlabs/instant-ngp/discussions/797
-
-		pbar = tqdm(total=len(root[0][2]))
-
-		frames = []
-
-		for camera, sensor in calibration(root):
-			pbar.update(1)
-
-			if (camera is None) or (sensor is None):
-				print('no camera or sensor found')
-				continue
-
+		if not _ignore_images:
 			# Check if label is in image folder
 			label = [str(f) for f in stems if(camera['label'] in str(f))]
 
 			if(len(label) == 0):
-				print('no matching label found for: {}'.format(camera['label']))
+				print('No matching image found for: {}'.format(camera['label']))
 				continue
-
-			del camera['label']
-			del camera['sensor_id']
-			del sensor['id']
 
 			imagePath = IMGFOLDER + '/' + label[0] + "." + IMGTYPE
 
@@ -337,22 +335,72 @@ if __name__ == "__main__":
 			camera["file_path"] = imagePath
 			camera["sharpness"] = sharpness(imagePath)
 
-			frame = sensor
-			frame.update(camera)
-			frames.append(frame)
+		del camera['label']
+		del camera['sensor_id']
+		del sensor['id']
+
+		frame = sensor
+		frame.update(camera)
+		frames.append(frame)
+	
+	if (region_mat is None) or (region_scale is None):
+		print('No bounding region found')
+		region = None
+	else:
+		if (scene_mat is not None) & (scene_scale is not None):
+			region_mat[:3,3] *= scene_scale # Scale the transform
+			region_mat = np.dot(scene_mat, region_mat) # Rotate the bbox to match the scene
+			region_scale *= scene_scale
+
+		region_mat = agiMat2Nerf(region_mat) # Convert to the coordinates
 		
-		out.update({"frames": frames})
+		region = dict(transform_matrix=region_mat, size=region_scale)
+	
+	return frames, region
 
-	out = central_point(out)
 
-	with open(args.out, "w") as f:
+if __name__ == "__main__":
+	args = parse_args()
+	XML_LOCATION = args.xml_in
+	IMGTYPE = args.imgtype
+	IMGFOLDER = args.imgfolder
+
+	files = list(Path(IMGFOLDER).glob('*.{}'.format(IMGTYPE)))
+	stems = list([f.stem for f in files])
+
+	# Check if the files path has images in it
+	if(len(files)==0) & (args.debug_ignore_images==False):
+		print('No images found in folder: {}'.format(IMGFOLDER))
+		exit()
+
+	out = dict()
+
+	out['aabb_scale'] = args.aabb_scale
+
+	with open(XML_LOCATION, "r") as f:
+		xml_root = ET.parse(f).getroot()
+		
+		# See issue for multi camera support
+		# https://github.com/NVlabs/instant-ngp/discussions/797
+
+		frames, region = calibration(xml_root, stems,
+			       			args.scale, args.no_scale, args.debug_ignore_images)
+
+	out['frames'] = frames
+
+	if args.no_center:
+		center = np.zeros(3)
+	else:
+		# Compute the center of attention
+		center = central_point(out)
+
+	# Set the offset and convert to list
+	for f in out["frames"]:
+		f["transform_matrix"][0:3,3] -= center
+		f["transform_matrix"] = f["transform_matrix"].tolist()
+	
+	with open(args.path, "w") as f:
 		json.dump(out, f, indent=4)
 	
-	if _plt:
-		# 3D plot the points and display them
-		fig = plt.figure()
-		ax = plt.axes(projection='3d')
-		for f in out['frames']:
-			t = np.array(f["transform_matrix"])[0:3,3]
-			ax.scatter3D(t[0], t[1], t[2], c='r')
-		plt.show()
+	if _plt & args.plot:
+		plot(out, center, region, args.camera_size)
---
+++ utils.py
@@ -0,0 +1,169 @@
+import numpy as np
+import cv2
+
+# Check if plot libraries are installed
+try:
+	from pytransform3d.plot_utils import plot_box
+	from pytransform3d.transform_manager import TransformManager
+
+	import pytransform3d.camera as pc
+	import pytransform3d.transformations as pytr
+
+	import matplotlib.pyplot as plt
+	from matplotlib.widgets import Slider, Button
+	_plt = True
+except ImportError as e:
+	print(e)
+	_plt = False
+
+
+###############################################################################
+# START
+# code taken from https://github.com/NVlabs/instant-ngp
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+
+def closest_point_2_lines(oa, da, ob, db): # returns point closest to both rays of form o+t*d, and a weight factor that goes to 0 if the lines are parallel
+	da = da / np.linalg.norm(da)
+	db = db / np.linalg.norm(db)
+	c = np.cross(da, db)
+	denom = np.linalg.norm(c)**2
+	t = ob - oa
+	ta = np.linalg.det([t, db, c]) / (denom + 1e-10)
+	tb = np.linalg.det([t, da, c]) / (denom + 1e-10)
+	if ta > 0:
+		ta = 0
+	if tb > 0:
+		tb = 0
+	return (oa+ta*da+ob+tb*db) * 0.5, denom
+
+def central_point(out):
+	# find a central point they are all looking at
+	print("computing center of attention...")
+	totw = 0.0
+	totp = np.array([0.0, 0.0, 0.0])
+	for f in out["frames"]:
+		mf = np.array(f["transform_matrix"])[0:3,:]
+		for g in out["frames"]:
+			mg = g["transform_matrix"][0:3,:]
+			p, w = closest_point_2_lines(mf[:,3], mf[:,2], mg[:,3], mg[:,2])
+			if w > 0.01:
+				totp += p*w
+				totw += w
+
+	totp /= totw
+	print("The center of attention is: {}".format(totp)) # the cameras are looking at totp
+
+	return totp
+
+def sharpness(imagePath):
+	image = cv2.imread(imagePath)
+	if image is None:
+		print("Image not found:", imagePath)
+		return 0
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	fm = cv2.Laplacian(gray, cv2.CV_64F).var()
+	return fm
+
+#END
+###############################################################################
+
+
+def reflect(axis, size=4):
+	_diag = np.ones(size)
+	_diag[axis] = -1
+	refl = np.diag(_diag)
+	return refl
+
+
+def agiMat2Nerf(mat, _reflect=True):
+	# return mat
+	M = np.array(mat)
+
+	# Swap axes
+	M = M[[2,0,1,3],:]
+
+	# if _reflect:
+	# Reflect Z and Y axes
+	M = ((M @ reflect(2)) @ reflect(1))
+
+	return M
+
+
+def draw_cameras(ax, out, camera_size):
+	# Plot the camera positions
+	for f in out['frames']:
+		sensor_size = np.array([f["w"], f["h"]])
+
+		intrinsic = np.eye(3)
+		intrinsic[0,0] = f["fl_x"]
+		intrinsic[1,1] = f["fl_y"]
+		intrinsic[0,2] = f["cx"] if "cx" in f else sensor_size[0] / 2.0
+		intrinsic[1,2] = f["cy"] if "cy" in f else sensor_size[1] / 2.0
+
+		cam_mat = np.array(f["transform_matrix"])
+
+		# Scale the camera position
+		# cam_mat[0:3,3] *= scale
+
+		# Reflect the camera back for plotting
+		cam_mat = cam_mat @ reflect(1) @ reflect(2)
+
+		pytr.plot_transform(ax, A2B=cam_mat, s=camera_size)
+		pc.plot_camera(ax, cam2world=cam_mat, M=intrinsic,
+						sensor_size=sensor_size,
+						virtual_image_distance=camera_size)
+		
+
+def plot(out, origin, region, camera_size=0.1):
+
+	# 3D plot the points and display them
+	fig = plt.figure()
+	
+	ax = plt.axes(projection='3d')
+
+	ax.set_xlabel('x')
+	ax.set_ylabel('z')
+	ax.set_zlabel('y')
+
+	fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
+	# Find the scene scale
+	P = [np.array(f["transform_matrix"])[0:3,3] for f in out['frames']]
+	pos_min = np.min(P, axis=0)
+	pos_max = np.max(P, axis=0)
+	# print("Scene size:", pos_max - pos_min)
+	center = (pos_max + pos_min) / 2.0
+	max_half_extent = max(pos_max - pos_min) / 2.0
+	# print("Max half extent:", max_half_extent)
+
+	# Plot the camera positions
+	draw_cameras(ax, out, camera_size)
+
+	# Plot the origin for reference
+	pytr.plot_transform(ax, A2B=np.eye(4), s=1)
+
+	if region is not None:
+		# Plot the bounding box
+		bbox_mat = region['transform_matrix']
+		bbox_mat = bbox_mat @ reflect(1) @ reflect(2)
+		bbox_mat[0:3,3] -= origin # Translate the bbox to match the center
+		plot_box(ax, size=region['size'], A2B=bbox_mat, color='r', alpha=0.5)
+
+	# Set the limits
+	ax.set_xlim((center[0] - max_half_extent, center[0] + max_half_extent))
+	ax.set_ylim((center[1] - max_half_extent, center[1] + max_half_extent))
+	ax.set_zlim((center[2] - max_half_extent, center[2] + max_half_extent))
+
+	# Create sliders to adjust scale of the scene
+	# slider_scale = Slider(plt.axes([0.25, 0.05, 0.65, 0.03]), 'Scale', 0.01, 10.0, valinit=1.0)
+	# plt.axes([0.25, 0.05, 0.65, 0.03])
+	# def update(val):
+	# 	scale = slider_scale.val
+	# 	draw_cameras(ax, out, scale)
+	# 	fig.canvas.draw_idle()
+
+	# slider_scale.on_changed(update)
+
+	plt.show()
+
+	
